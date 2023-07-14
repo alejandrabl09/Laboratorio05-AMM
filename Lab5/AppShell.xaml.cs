ed228cc559314686f87ba9cef63c9e31d39a2459
alejandrabl09 <aleblasdc09@gmail.com>
2023-07-13 20:44:05 -0500

    first commit

---
+++ Lab5/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Lab5.ViewModels;
+using Lab5.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Lab5
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Lab5/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Lab5.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Lab5.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
